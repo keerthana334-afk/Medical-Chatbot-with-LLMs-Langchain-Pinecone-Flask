371953c93c45df2e3ca38ea62eba89849eec048b
Keerthana S <keerthanas030304@gmail.com>
2025-11-02 00:51:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Keerthana S",
+    author_email="keerthanas030304@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
